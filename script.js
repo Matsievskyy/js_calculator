dccf417a50423fe2cdcb870f023c49b23e12e6a2
Matsievskyy <48979055+Matsievskyy@users.noreply.github.com>
2019-05-18 14:51:05 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,3 @@
-const oneBtn = document.getElementById('calc-one');
-const twoBtn = document.getElementById('calc-two');
-const threeBtn = document.getElementById('calc-three');
-const fourBtn = document.getElementById('calc-four');
-const fiveBtn = document.getElementById('calc-five');
-const sixBtn = document.getElementById('calc-six');
-const sevenBtn = document.getElementById('calc-seven');
-const eightBtn = document.getElementById('calc-eight');
-const nineBtn = document.getElementById('calc-nine');
-const zeroBtn = document.getElementById('calc-zero');
 
 const decimalBtn = document.getElementById('calc-decimal');
 const clearlBtn = document.getElementById('calc-clear');
